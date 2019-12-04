d38eb11d517540b8282e5cd6899dd586858360ff
senkaPro <senkaPro@users.noreply.github.com>
2019-12-04 04:06:33 +0000

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -1,17 +1,22 @@
 """
 Making Half-Pyramid with given height
 """
+
+
 def get_int():
+    """Function to get integer and return false otherwise"""
     imp = input("Height: ")
     if imp.isdigit():
         return int(imp)
     else:
         return False
 
+
+# Looping till we get number between 1 and 8 and than drawing the half-pyramid
 while True:
     height = get_int()
     if height != False and height > 0 and height <= 8:
-        for i in range(1,height+1):
-            print(" " * (height - i),end="")
+        for i in range(1, height+1):
+            print(" " * (height - i), end="")
             print("#" * (i))
         break
