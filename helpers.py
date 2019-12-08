2df2b2c8a599de094b26070695f7f16c7a48c0ff
senkaPro <senkaPro@users.noreply.github.com>
2019-12-08 15:39:55 +0000

    automated commit by check50 [check50=True]

--- helpers.py
+++ helpers.py
@@ -2,15 +2,15 @@ def lines(a, b):
     """Return lines in both a and b"""
     # set of unique similar lines
     uniq = set()
-
+    # list of sentences in a
     lst_1 = a.split('\n')
+    # list of sentences in b
     lst_2 = b.split('\n')
 
+    # loop throught the sentences in a
     for line in lst_1:
-        lst = []
-        lst.append(line)
         for line1 in lst_2:
-            if line1 in lst:
+            if line1 in lst_1:
                 uniq.add(line)
 
     return list(uniq)
