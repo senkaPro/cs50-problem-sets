9ca99987c319daab424ed630630527b75c128048
senkaPro <senkaPro@users.noreply.github.com>
2020-01-07 23:45:41 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -132,7 +132,7 @@ bool vote(int voter, int rank, string name)
     {
        for (int j = 0; j < candidate_count; j++)
        {
-           if (strcmp(candidate[i].name, name) == 0)
+           if (strcmp(candidates[i].name, name) == 0)
            {
                i = voter;
                j = rank;
