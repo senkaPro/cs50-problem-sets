3c947d05105566e165d313302e7f7a4d6196dcd0
senkaPro <senkaPro@users.noreply.github.com>
2020-01-08 02:19:14 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -115,7 +115,7 @@ void record_preferences(int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count - i; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
             preferences[ranks[i]][ranks[i + j]]++;
         }
