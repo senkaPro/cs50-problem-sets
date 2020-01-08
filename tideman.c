e39eb0f233df29288e701b70a2c609e7d17188b1
senkaPro <senkaPro@users.noreply.github.com>
2020-01-08 02:23:06 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -145,7 +145,20 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    // TODO
+    pair k;
+    for (int i = 0; i < pair_count; i++)
+    {
+        for (int j = i + 1; j < pair_count; j++)
+        {
+            if (preferences[pairs[i].winner][pairs[i].loser] < preferences[pairs[j].winner][pairs[j].loser])
+            {
+
+                k = pairs[i];
+                pairs[i] = pairs[j];
+                pairs[j] = k;
+            }
+        }
+    }
     return;
 }
 
