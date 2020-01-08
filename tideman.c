cca1cbb01eedc54495cb951d4194a5f9c6cfa2b4
senkaPro <senkaPro@users.noreply.github.com>
2020-01-08 02:23:53 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -165,7 +165,13 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    // TODO
+    for (int i = 0; i < pair_count; i++)
+    {
+        if (!check_cycle(pairs[i].winner, pairs[i].loser))
+        {
+            locked[pairs[i].winner][pairs[i].loser] = true;
+        }
+    }
     return;
 }
 
